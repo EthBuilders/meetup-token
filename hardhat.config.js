977f47feb70a868e0bd1aacbe6ad07a3b9e99d2f
Chan-Ho Suh <chanhosuh@gmail.com>
2020-12-07 14:23:16 -0500

    Update hardhat config
    - we are using truffle and ethers, but not waffle.
    - downgrade solidity version
    - add some network settings

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,34 @@
-require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-truffle5");
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.7.3",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      timeout: 1000000,
+    },
+    mainnet: {
+      url: "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY,
+      gasPrice: 72e9,
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "",
+      },
+      timeout: 1000000,
+    },
+    kovan: {
+      url: "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY,
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "",
+      },
+      timeout: 1000000,
+    },
+  },
+  solidity: {
+    version: "0.6.11",
+  },
+  mocha: {
+    timeout: 1000000,
+  },
 };
-
